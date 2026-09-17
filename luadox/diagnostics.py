873cfb28a2cdb693512175b58cc5d1bbc04d3e35
ivan-vaitusionak-rightware <ivan.vaitusionak@rightware.com>
2026-09-17 08:55:03 +0200

    Report undocumented @enum members under a granular undocumented-enum-members category (#11)

--- luadox/diagnostics.py
+++ luadox/diagnostics.py
@@ -43,7 +43,7 @@ class Diagnostics:
     """
 
     CATEGORIES = frozenset({'snippets', 'references', 'conflicts', 'structure', 'untyped',
-                            'undocumented'})
+                            'undocumented-enum-members'})
 
 
     def __init__(self, allowed: Set[str]):
